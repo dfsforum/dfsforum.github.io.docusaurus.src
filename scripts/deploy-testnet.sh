3aaf7c22b0afa87f7d3d145bb6ee674ef218aea8
Byron Gibson <fbg111@gmail.com>
2024-06-07 15:55:05 -0400

    make script generic

--- scripts/deploy-testnet.sh
+++ scripts/deploy-testnet.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-
-rsync -C --exclude=.DS* --recursive -v -a build/ -e ssh root@165.232.86.61:/var/lib/docker/volumes/docusaurus_warp_docs/_data
+# deploy docusaurus /build dir to test/staging/prod server via ssh
+rsync -C --exclude=.DS* --recursive -v -a build/ -e ssh $@
