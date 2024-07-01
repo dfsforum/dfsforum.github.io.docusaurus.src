9718df6bc0d58da766433b92dbd00898c55754ba
Byron Gibson <fbg111@gmail.com>
2024-07-01 11:33:01 -0700

    site title->DFSF; remove docs; change site link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,20 +3,20 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'DFSF',
+  tagline: 'Decentralized Financial Stability Forum',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://dfsforum.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'DFSForum', // Usually your GitHub org/user name.
+  projectName: 'DFSForum', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -33,20 +33,14 @@ const config: Config = {
     [
       'classic',
       {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
+        /*docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+        },*/
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -56,24 +50,31 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/dfsforum_235x49.png',
     navbar: {
-      title: 'My Site',
+      title: 'DFSForum',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'DFSForum',
+        //src: 'img/dfsforum-logo-light.png',
+        src: 'img/data-1293312.svg',
       },
       items: [
-        {
+        /*{
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
           label: 'Tutorial',
-        },
+        },*/
         {to: '/blog', label: 'Blog', position: 'left'},
+        {to: '/events', label: 'Events', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/dfsforum',
           label: 'GitHub',
+          position: 'right',
+        },
+        {
+          href: 'https://zotero.org/dfsforum',
+          label: 'Zotero',
           position: 'right',
         },
       ],
@@ -82,11 +83,32 @@ const config: Config = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: 'About',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: 'Home',
+              to: '/',
+            },
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'Events',
+              to: '/events',
+            },
+          ],
+        },
+        {
+          title: 'Research',
+          items: [
+            {
+              label: 'Github',
+              to: 'https://github.com/dfsforum',
+            },
+            {
+              label: 'Zotero',
+              href: 'https://www.zotero.org/groups/dfsforum',
             },
           ],
         },
@@ -94,34 +116,25 @@ const config: Config = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: 'Google Groups',
+              href: 'https://groups.google.com/u/3/g/dfsforum',
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: 'Reddit',
+              href: 'https://reddit.com/r/dfsforum',
+            },
+            {
+              label: 'Telegram',
+              href: 'https://t.me/+0bFYLI4PsotmMDYx',
             },
             {
               label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://twitter.com/dfsforum',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} DFSForum.`,
     },
     prism: {
       theme: prismThemes.github,
